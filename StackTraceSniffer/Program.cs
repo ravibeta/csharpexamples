8a29dbc0f84cbe508a96d81657c2509abd9c727a
Ravi Rajamani <ravibeta@hotmail.com>
2013-06-03 08:00:45 -0700

    update

--- StackTraceSniffer/Program.cs
+++ StackTraceSniffer/Program.cs
@@ -19,16 +19,21 @@ namespace StackTraceSniffer
             string ret = string.Empty;
             using (var fs = new FileStream(dumpFileName,FileMode.Open))
             {
-                var offset = 0;
                 var size = 1024;
-                var bytes = new Byte[1024];
+                var bytes = new Byte[1024]; // The start signature may not be "DUMP"
                 var s = fs.Read(bytes, 0, size);
-                for (int i = 0; i < 4; i++)
-                {
-                    Console.WriteLine(BitConverter.ToChar(bytes, offset + i));
-                }
+                var numStreams = BitConverter.ToUInt32(bytes, 8);
+                var rvaStream = BitConverter.ToUInt32(bytes, 12);
+                var exceptionStreamOffset = rvaStream + 12; // skip first directory;
+                
+                var rvaExceptionStream = BitConverter.ToUInt32(bytes, (int)exceptionStreamOffset + 8); 
+                var contextLocationOffset = rvaExceptionStream + 32 + 128; // based on MINIDUMP_EXCEPTION
+                var contextSize = BitConverter.ToUInt32(bytes, (int)contextLocationOffset); // MINIDUMP_LOCATION_DESCRIPTOR
+                var contextOffset = BitConverter.ToUInt32(bytes, (int)contextLocationOffset + 4); // RVA
+
+                // read the stack pointer from context 
+                
             }
-// read the stack pointer from context
             return ret;
         }
     }
