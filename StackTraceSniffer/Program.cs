f9505448c45f5bd292f66dde09199dcebb2a2c4c
Ravi Rajamani <ravibeta@hotmail.com>
2013-06-03 20:07:30 -0700

    update

--- StackTraceSniffer/Program.cs
+++ StackTraceSniffer/Program.cs
@@ -10,8 +10,14 @@ namespace StackTraceSniffer
     {
         static void Main(string[] args)
         {
-            var stacktrace = ReadDumpFile(args.First());
-            Console.WriteLine(stacktrace);
+            try
+            {
+                var stacktrace = ReadDumpFile(args.First());
+                Console.WriteLine(stacktrace);
+            }
+            catch (Exception e)
+            {
+            }
         }
 
         private static string ReadDumpFile(string dumpFileName)
@@ -19,19 +25,20 @@ namespace StackTraceSniffer
             string ret = string.Empty;
             using (var fs = new FileStream(dumpFileName,FileMode.Open))
             {
-                var size = 1024;
-                var bytes = new Byte[1024]; // The start signature may not be "DUMP"
+                var size = 4096;
+                var bytes = new Byte[4096]; // The start signature may not be "DUMP"
                 var s = fs.Read(bytes, 0, size);
                 var numStreams = BitConverter.ToUInt32(bytes, 8);
                 var rvaStream = BitConverter.ToUInt32(bytes, 12);
                 var exceptionStreamOffset = rvaStream + 12; // skip first directory;
                 
-                var rvaExceptionStream = BitConverter.ToUInt32(bytes, (int)exceptionStreamOffset + 8); 
+                var rvaExceptionStream = BitConverter.ToUInt32(bytes, (int)exceptionStreamOffset + (int)8); 
                 var contextLocationOffset = rvaExceptionStream + 32 + 128; // based on MINIDUMP_EXCEPTION
                 var contextSize = BitConverter.ToUInt32(bytes, (int)contextLocationOffset); // MINIDUMP_LOCATION_DESCRIPTOR
                 var contextOffset = BitConverter.ToUInt32(bytes, (int)contextLocationOffset + 4); // RVA
 
                 // read the stack pointer from context 
+                var stackPointerOffset = BitConverter.ToUInt32(bytes, (int)contextOffset + 0xc4);
                 
             }
             return ret;
