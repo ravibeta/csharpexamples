d4a6ae03af2e81b30ef76902702c8a185ec80587
Ravi Rajamani <ravibeta@hotmail.com>
2013-06-10 06:55:35 -0700

    stack trace service

---
+++ StackTraceService/StackService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Debuggers.DbgEng;
+
+namespace StackTraceService
+{
+    public class StackService : IStackService
+    {
+        public IEnumerable<string> GetStackTrace(string filename)
+        {
+            try
+            {
+                using (var proxy = new DebugClient())
+                {
+
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                var str = e.Message;
+                Console.WriteLine(str);
+            }
+            return null;
+        }
+    }
+}
