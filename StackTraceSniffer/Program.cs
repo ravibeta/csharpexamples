507aa9b795f62f8f487943c0c77c9e7ef900915e
Ravi Rajamani <ravibeta@hotmail.com>
2013-06-19 15:59:14 -0700

    try hand parse dump

--- StackTraceSniffer/Program.cs
+++ StackTraceSniffer/Program.cs
@@ -16,6 +16,7 @@ namespace StackTraceSniffer
             try
             {
                 var frames = GetStackTrace(args.First());
+                // var frames = ReadDumpFile(args.First());
                 frames.ToList().ForEach(x => Console.WriteLine(x));
             }
             catch (Exception e)
@@ -59,6 +60,33 @@ namespace StackTraceSniffer
         {
             var str = e.Output;
             str.Split(new char[] { '\n' }).ToList().ForEach(x => frames.Add(x));
+        }
+
+        private static string ReadDumpFile(string dumpFileName)        
+        {            
+            string ret = string.Empty;            
+            using (var fs = new FileStream(dumpFileName,FileMode.Open))            
+            {                
+                var size = 4096;                
+                var bytes = new Byte[4096]; // The start signature may not be "DUMP"                
+                var s = fs.Read(bytes, 0, size);                
+                var numStreams = BitConverter.ToUInt32(bytes, 8);                
+                var rvaStream = BitConverter.ToUInt32(bytes, 12);
+                if (rvaStream + 24 < bytes.Length)
+                {
+                    var exceptionStreamOffset = rvaStream + 12; // skip first directory;  
+                    var rvaExceptionStream = BitConverter.ToUInt32(bytes, (int)exceptionStreamOffset + (int)8);
+                    if (rvaExceptionStream + 32 + 128 + 8 < fs.Length)
+                    {
+                        var contextLocationOffset = rvaExceptionStream + 32 + 128; // based on MINIDUMP_EXCEPTION                
+                        var contextSize = BitConverter.ToUInt32(bytes, (int)contextLocationOffset); // MINIDUMP_LOCATION_DESCRIPTOR                
+                        var contextOffset = BitConverter.ToUInt32(bytes, (int)contextLocationOffset + 4); // RVA                
+                    }
+                    // read the stack pointer from context                 
+                    var stackPointerOffset = BitConverter.ToUInt32(bytes, (int)contextOffset + 0xc4); // _CEDUMP_ELEMENT_LIST
+                }
+            }
+            return ret;
         }
     }
 }
