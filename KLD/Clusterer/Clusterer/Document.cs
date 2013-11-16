ab5ae911ad9f05ac7c9411994fa9b757bc000134
Ravi Rajamani <ravibeta@hotmail.com>
2013-11-16 10:41:43 -0800

    Kulback-Leibler based clusterer
    initial checkin

---
+++ KLD/Clusterer/Clusterer/Document.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clusterer
+{
+    public class Document 
+    {
+        // based on Kullback-Leibler 
+        public List<decimal> Probabilities { get; set; }
+
+        public const decimal EPSILONPROBABILITY = 0.01m;
+
+        public List<string> Vocabulary { get; set; }
+        
+        public Dictionary<string, int> FreqDistributionOfTermsFromVocabulary { get; set; }
+
+
+        public decimal GetProbability(string term)
+        {
+            decimal probability = EPSILONPROBABILITY;
+            if (FreqDistributionOfTermsFromVocabulary.Keys.Contains(term) == false)
+            {
+                return probability;
+            }
+
+            int total = 0;
+            foreach (var kvp in FreqDistributionOfTermsFromVocabulary)
+            {
+                total += kvp.Value;
+            }
+
+            if (total != 0)
+            {
+                probability = GetScalingFactor() * Convert.ToDecimal(FreqDistributionOfTermsFromVocabulary[term] / total);
+            }
+
+            return probability; 
+        }
+
+        public decimal GetScalingFactor()
+        {
+            var numUnknownTerms = Vocabulary.Count() - FreqDistributionOfTermsFromVocabulary.Count();
+            Debug.Assert(numUnknownTerms > 0);
+            return 1 - numUnknownTerms * EPSILONPROBABILITY;
+        }
+    }
+}
---
+++ KLD/Clusterer/Clusterer/IDistance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clusterer
+{
+    public interface IDistance
+    {
+        decimal GetDistance(Document di, Document dj);
+    }
+}
