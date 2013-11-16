12017d1b40dcdf5dc660956a4d41e82a1fec2c3e
Ravi Rajamani <ravibeta@hotmail.com>
2013-11-16 11:00:51 -0800

    add test

---
+++ KLD/Clusterer/ClustererTest/ClustererTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clusterer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClustererTest
+{
+    [TestClass]
+    public class ClustererTest
+    {
+        [TestMethod]
+        public void DocumentTest()
+        {
+            var document = new Document();
+            document.Vocabulary = new List<string>() { "apple", "banana", "orange" };
+            document.FreqDistributionOfTermsFromVocabulary = new Dictionary<string, int>() { };
+            document.FreqDistributionOfTermsFromVocabulary["apple"] = 2;
+            document.FreqDistributionOfTermsFromVocabulary["banana"] = 3;
+            document.Probabilities = new List<decimal>();
+            foreach (var term in document.Vocabulary)
+            {
+                document.Probabilities.Add(document.GetProbability(term));
+            }
+            var total = document.Probabilities.Sum();
+            Assert.IsTrue(total == Convert.ToDecimal(1.0));
+        }
+    }
+}
