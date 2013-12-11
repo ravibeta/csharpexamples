f8d7b9d8b05184fa913bb874893c488c88d8030b
Ravi Rajamani <ravibeta@hotmail.com>
2013-12-10 21:49:26 -0800

    Create IndexedNumber.cs

---
+++ IndexedNumber.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexSum.Model
+{
+    public class IndexedNumber
+    {
+        public int Number { get; set; }
+        public int Index { get; set; }
+        public bool Used { get; set; }
+    }
+}
