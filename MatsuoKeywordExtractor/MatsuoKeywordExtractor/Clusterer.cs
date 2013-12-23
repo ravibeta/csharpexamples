8d6d4dbc0ce8808da8f530d6a1a370ad7277f411
Ravi Rajamani <ravibeta@hotmail.com>
2013-12-23 10:31:24 -0800

    add co-occurrence matrix in clusterer

---
+++ MatsuoKeywordExtractor/MatsuoKeywordExtractor/Clusterer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatsuoKeywordExtractor
+{
+    public class Clusterer
+    {
+        public Dictionary<string, int> WordCount { get; set; }
+        public List<KeyValuePair<string, int>> FrequentTerms { get; set; }
+        public string[] sentences { get; set; }
+        public int[,] cooccurenceMatrix { get; set; }
+
+        public void Classify()
+        {
+        }
+
+
+        private int[,] PopulateCooccurrenceMatrix(SortedDictionary<string, int> dict, string[] sentences)
+        {
+
+            var topTerms = dict.OrderByDescending(x => x.Value);
+            var cutoff = topTerms.Sum(x => x.Value) * 0.3d;
+            var sum = 0.0d;
+            var frequentTerms = topTerms.TakeWhile(x => { sum += x.Value; return sum < cutoff; }).ToList();
+            FrequentTerms = frequentTerms;
+            var matrix = new int[topTerms.Count(), frequentTerms.Count()];
+            int i = -1;
+            foreach (var word in topTerms)
+            {
+                i++;
+                int j = -1;
+                foreach (var pair in frequentTerms)
+                {
+                    j++;
+                    if (word.Key != pair.Key)
+                    {
+                        if (matrix[i, j] != 0)
+                        {
+                            matrix[j, i] = matrix[i, j];
+                            continue;
+                        }
+                        if (matrix[j, i] != 0)
+                        {
+                            matrix[i, j] = matrix[j, i];
+                            continue;
+                        }
+                        var count = sentences.Count(x => x.Contains(word.Key) && x.Contains(pair.Key));
+                        matrix[i, j] = count;
+                    }
+                }
+            }
+
+            return matrix;
+
+        }
+    }
+}
--- MatsuoKeywordExtractor/MatsuoKeywordExtractor/Program.cs
+++ MatsuoKeywordExtractor/MatsuoKeywordExtractor/Program.cs
@@ -15,14 +15,26 @@ namespace MatsuoKeywordExtractor
             {
                 if (args == null || args.Count() == 0) return;
                 var fileContent = File.ReadAllText(args[0]);
-                var words = fileContent.Split(new char[] { '?', '!', '.', ',', '/', ';', ':', '<', '>', '{', '}', '[', ']', '(', ')', '-', '\'', '\\', '`', '@', '#', '$', '%', '^', '&', '*', '=' });
+                var sentences = fileContent.Split(new char [] {'?', '.' ,'!'});
+                var words = fileContent.Split(new char[] { '?', '!', '.', ',', '/', ';', ':', '<', '>', '{', '}', '[', ']', '(', ')', '-', '\'', '\\', '`', '@', '#', '$', '%', '^', '&', '+', '*', '=' });
                 // Step1 : Preprocessing and stemming
                 // Step2 : Selection of frequent terms
                 // Step3 : Clustering frequent terms
                 // Step4 : Calculation of expected probability
                 // Step5 : Calculation of chi-square value
                 // Step6 : Output Keywords
-
+                var dict = new SortedDictionary<string, int>();
+                var stopWords = File.ReadAllText(@"StopWords.txt").Split(new char[] { ',' });
+                foreach (var word in words)
+                {
+                    if (stopWords.Contains(word) == false)
+                    {
+                        if (dict.ContainsKey(word))
+                            dict[word] += 1;
+                        else
+                            dict.Add(word, 1);
+                    }
+                }
             }
             catch(Exception e)
             {
