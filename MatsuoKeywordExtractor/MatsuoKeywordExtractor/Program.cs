2e896246cb4ae43ef557a9dd30f4d2e129de5010
Ravi Rajamani <ravibeta@hotmail.com>
2013-12-23 07:00:09 -0800

    Matsuo implementation

---
+++ MatsuoKeywordExtractor/MatsuoKeywordExtractor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatsuoKeywordExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args == null || args.Count() == 0) return;
+                var fileContent = File.ReadAllText(args[0]);
+                var words = fileContent.Split(new char[] { '?', '!', '.', ',', '/', ';', ':', '<', '>', '{', '}', '[', ']', '(', ')', '-', '\'', '\\', '`', '@', '#', '$', '%', '^', '&', '*', '=' });
+                // Step1 : Preprocessing and stemming
+                // Step2 : Selection of frequent terms
+                // Step3 : Clustering frequent terms
+                // Step4 : Calculation of expected probability
+                // Step5 : Calculation of chi-square value
+                // Step6 : Output Keywords
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
