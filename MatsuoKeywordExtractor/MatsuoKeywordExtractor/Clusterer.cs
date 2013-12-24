57492752987765291b7d12c3228b96be1b1eb4c5
Ravi Rajamani <ravibeta@hotmail.com>
2013-12-23 19:17:18 -0800

    add test

--- MatsuoKeywordExtractor/MatsuoKeywordExtractor/Clusterer.cs
+++ MatsuoKeywordExtractor/MatsuoKeywordExtractor/Clusterer.cs
@@ -15,16 +15,18 @@ namespace MatsuoKeywordExtractor
 
         public void Classify()
         {
+            cooccurenceMatrix = PopulateCooccurrenceMatrix(WordCount, sentences);
         }
 
 
-        private int[,] PopulateCooccurrenceMatrix(SortedDictionary<string, int> dict, string[] sentences)
+        internal int[,] PopulateCooccurrenceMatrix(Dictionary<string, int> dict, string[] sentences)
         {
 
             var topTerms = dict.OrderByDescending(x => x.Value);
             var cutoff = topTerms.Sum(x => x.Value) * 0.3d;
             var sum = 0.0d;
-            var frequentTerms = topTerms.TakeWhile(x => { sum += x.Value; return sum < cutoff; }).ToList();
+            int entries = 0;
+            var frequentTerms = topTerms.TakeWhile(x => { entries++;  sum += x.Value; return sum < cutoff || entries == 1; }).ToList();
             FrequentTerms = frequentTerms;
             var matrix = new int[topTerms.Count(), frequentTerms.Count()];
             int i = -1;
@@ -37,16 +39,16 @@ namespace MatsuoKeywordExtractor
                     j++;
                     if (word.Key != pair.Key)
                     {
-                        if (matrix[i, j] != 0)
-                        {
-                            matrix[j, i] = matrix[i, j];
-                            continue;
-                        }
-                        if (matrix[j, i] != 0)
-                        {
-                            matrix[i, j] = matrix[j, i];
-                            continue;
-                        }
+                        //if (matrix[i, j] != 0)
+                        //{
+                        //    matrix[j, i] = matrix[i, j];
+                        //    continue;
+                        //}
+                        //if (matrix[j, i] != 0)
+                        //{
+                        //    matrix[i, j] = matrix[j, i];
+                        //    continue;
+                        //}
                         var count = sentences.Count(x => x.Contains(word.Key) && x.Contains(pair.Key));
                         matrix[i, j] = count;
                     }
---
+++ MatsuoKeywordExtractor/MatsuoKeywordExtractor/IDistance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatsuoKeywordExtractor
+{
+    public interface IDistance
+    {
+        double GetDistance(Word di, Word dj);
+    }
+}
