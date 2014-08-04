70f35315876e4a553b9e698cedc766bd742b15d4
Ravi Rajamani <ravibeta@hotmail.com>
2014-08-03 19:36:50 -0700

    initial commit SplunkLite.Net
    Project to write a lightweight forwarder->indexer->Searchhead in .Net

---
+++ SplunkLite.Net/SplunkLite.Net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplunkLite.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
