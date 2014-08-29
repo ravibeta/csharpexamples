a70a35ccd3e25ff2b51b52d4b4dfb9f9177732d8
Ravi Rajamani <ravibeta@hotmail.com>
2014-08-28 20:06:56 -0700

    add table for controller
    add table for controller

--- SplunkLite.Net/SplunkLite.Net.Rest/Controllers/HomeController.cs
+++ SplunkLite.Net/SplunkLite.Net.Rest/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SplunkLite.Net.Rest;
 
 namespace SplunkLite.Net.Rest.Controllers
 {
     public class HomeController : Controller
     {
+        private SplunkLite.Net.Rest.EventsEntities entities = new SplunkLite.Net.Rest.EventsEntities();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to SplunkLite.Net";
@@ -27,6 +30,12 @@ namespace SplunkLite.Net.Rest.Controllers
             ViewBag.Message = "Please email rravishankar@gmail.com";
 
             return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "returns all events";
+            return View(entities.Events);
         }
     }
 }
