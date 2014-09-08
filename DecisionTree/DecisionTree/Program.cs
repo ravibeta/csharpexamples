435f910a70924e489cc13c7de0c6087ae4ec13ce
Ravi Rajamani <ravibeta@hotmail.com>
2014-09-07 20:15:45 -0700

    add decision tree implementation

---
+++ DecisionTree/DecisionTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static Dictionary<string, int> GetComputedColumn(Dictionary<string, int> itemBuyerAge)
+        {
+            // TODO: use Shannon's measure
+            // H(X) = -∑ P(xi) log(P(xi))
+            return null;
+        }
+    }
+}
